bf485519f0bfc4ee5b90329ec65b5c42a3df09f2
kimsta <stahlberg.kim@gmail.com>
2018-11-10 13:02:29 +0200

    Update data/week2_dataset.R

--- data/week2_dataset.R
+++ data/week2_dataset.R
@@ -1,3 +1,6 @@
 #Kim Ståhlberg
 #09112018
 #This file is for week two exercise
+
+learning_2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header = T)
+dim(learning_2014)
